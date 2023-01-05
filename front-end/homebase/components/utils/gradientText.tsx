b35fc7a736c81e8a3681541527f88518b5c360ed
theom95 <theodore.mitchell95@gmail.com>
2023-01-04 19:56:27 -0600

    nuked project

--- front-end/homebase/components/utils/gradientText.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-const GradientText = (props) => {
-  const [gradient, setGradient] = useState("blue");
-
-  return <div style={{ color: gradient }}>{props.children}</div>;
-};
-
-export default GradientText;
